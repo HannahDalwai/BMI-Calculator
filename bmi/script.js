61349d25aeb680cb33bea9674769b5aec34bce2b
Hannah Dalwai <hannahdalwai4@gmail.com>
2021-11-29 14:52:20 +0200

    teamwork

---
+++ bmi/script.js
@@ -0,0 +1,22 @@
+let sum = "";
+
+function addToSum(operator){
+    sum = sum ;
+    console.log(sum);
+    document.getElementById("output").innerText = sum;
+}
+
+function evaluateSum(){
+    sum = eval(sum);
+    document.getElementById("output").innerText = sum;
+}
+
+function clearSum(){
+sum = "";
+document.getElementById("output").innerText = sum;
+}
+
+function deleteChar(){
+    sum = sum.slice(0, -1);
+    document.getElementById("output").innerText = sum;
+}
